ad74fb0439d553859ea3dec70732f0e3743c3efe
hideincode <hideincode@qq.com>
2019-04-28 20:28:32 +0800

    无重复的快排改名字以对应思路. 有重复的Quick3way

--- src/Sorting/Quick.java
+++ src/Sorting/Quick.java
@@ -33,10 +33,10 @@ public class Quick {
         if (low >= high) {
             return;
         }
-        int index = partition(a, low, high);
+        int pivotIndex = getPivotIndex(a, low, high);
 
-        sort(a, low, index - 1);
-        sort(a, index + 1, high);
+        sort(a, low, pivotIndex - 1);
+        sort(a, pivotIndex + 1, high);
     }
 
 
@@ -46,49 +46,49 @@ public class Quick {
      * 1.准备两个计数器,分两个方向->和<-在数组中找.
      * 2.两个计数器都不能越界
      * 3.左计数器永远不能大于右计数器
-     * 4.左边找到比切点值小的,和右边对换.右边计数器同理.
-     * 5.把找出来的切点值和初始值对换.
+     * 4.左边找到比轴小的,和右边对换.右边计数器同理.
+     * 5.把找出来的轴和初始值对换.
      *
      * @param a    数组
      * @param low  最小下标
      * @param high 最大下标
-     * @return 切点下标
+     * @return 轴的下标
      */
-    private static int partition(Comparable[] a, int low, int high) {
+    private static int getPivotIndex(Comparable[] a, int low, int high) {
 
         Comparable partitionVal = a[low];
 
-        int leftCounter = low;
-        int rightCounter = high + 1;
+        int leftIndex = low;
+        int rightIndex = high + 1;
 
         while (true) {
 
-            while (less(a[++leftCounter], partitionVal)) {
-                if (leftCounter == high) {
+            while (less(a[++leftIndex], partitionVal)) {
+                if (leftIndex == high) {
                     break;
                 }
 
             }
 
-            while (less(partitionVal, a[--rightCounter])) {
-                if (rightCounter == low) {
+            while (less(partitionVal, a[--rightIndex])) {
+                if (rightIndex == low) {
                     break;
                 }
             }
 
             //左右相遇,找到切点
-            if (leftCounter >= rightCounter) {
+            if (leftIndex >= rightIndex) {
                 break;
             }
 
 
             //内循环跳出时说明找到了可以交换的值.
-            exch(a, leftCounter, rightCounter);
+            exch(a, leftIndex, rightIndex);
         }
 
         //一定要返回右边的计数器啊,因为
-        exch(a, low, rightCounter);
-        return rightCounter;
+        exch(a, low, rightIndex);
+        return rightIndex;
     }
 
 
