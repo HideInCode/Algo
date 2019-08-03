00d50f8e662e79d62b6940798e5303b6eb917258
hideincode <hideincode@qq.com>
2019-08-03 22:18:19 +0800

    当年高中欠下的,现在都要还回来啦. kmp dfa实现 利用已匹配的部分子串,记录状态,转移状态.实现了一个能自动找到模式串中的下一坐标的数组. 一般用来处理自我重复性数据,如aaab匹配aaaaaaaab,特别适合用于匹配二进制流之类的.

---
+++ src/Strings/KMP.java
@@ -0,0 +1,77 @@
+package Strings;
+
+/**
+ * Knuth-Morris-Pratt算法
+ * 作用:提高暴力搜索时出现自我重复性的模式字符串的效率 如用aaaaab匹配aaaaaaaaaaaaaab
+ * 使用场景:处理二进制流
+ * 复杂度:时间O(txt.length+pat.length) 空间:O(R*pat.length)
+ */
+public class KMP {
+    private int R = 256;// 当前字符集字符个数
+    private int[][] dfa; //确定有限自动状态机
+    private String pat;//匹配模式
+
+
+    /**
+     * 构造dfa,储存的是状态,也是模式字符串指针下一个移动的位置
+     * 1.匹配失败:此时模式串指到patIndex时,考虑正常回退的情况.
+     * 如果模式指针都回退,文本指针回退后右移再匹配下一位.获取dfa状态.这时的pat[1]到pat[patIndex-1]是已知的.但是我们还要重新扫描一遍.
+     * 因此,我们可以找到pat:1->patIndex-1的dfa的状态,来替换掉当前状态.即initIndex = dfa[patIndex][initIndex],把上一次的转态转移到下一次来
+     * 2.匹配成功:就是开始匹配pat串的下一位即dfa[c][patIndex] = patIndex+1
+     *
+     * @param pat 匹配模式串
+     */
+    public KMP(String pat) {
+        this.pat = pat;
+
+        //开始构造一个dfa 这个算法的核心
+        dfa = new int[R][pat.length()];
+        dfa[pat.charAt(0)][0] = 1;//第一位匹配时,下一状态一定是1
+        for (int initIndex = 0, patIndex = 1; patIndex < pat.length(); patIndex++) {
+
+            //匹配失败,已经匹配过的状态保留
+            for (int c = 0; c < R; c++) {
+                dfa[c][patIndex] = dfa[c][initIndex];
+            }
+
+            //匹配成功,下个要匹配的模式字符肯定是patIndex+1
+            dfa[pat.charAt(patIndex)][patIndex] = patIndex + 1;
+
+            //更新重启位置,状态转移
+            initIndex = dfa[pat.charAt(patIndex)][initIndex];
+
+        }
+    }
+
+    /**
+     * 有了dfa提供下个模式指针的位置,一切都简单了
+     *
+     * @param txt 匹配文本
+     * @return 第一匹配到的位置
+     */
+    public int search(String txt) {
+        int txtIndex;
+        int patIndex;
+
+        for (txtIndex = 0, patIndex = 0; txtIndex < txt.length() && patIndex < pat.length(); txtIndex++) {
+            //利用自动机来找到下个模式串指针的位置
+            patIndex = dfa[txt.charAt(txtIndex)][patIndex];
+            System.out.println(patIndex);
+        }
+
+        //模式字符串匹配到结尾,说明找到了
+        if (patIndex == pat.length()) {
+            return txtIndex - patIndex;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        KMP kmp = new KMP("dfa");
+        String txt = "qwerftghjkpoiuytrdasdfaghjm,jhgfdsasdfgh";
+
+        int search = kmp.search(txt);
+        System.out.println(search);
+    }
+}
--- src/Strings/SimpleSearch.java
+++ src/Strings/SimpleSearch.java
@@ -4,37 +4,57 @@ public class SimpleSearch {
 
     private static final int END_POSITION = -1;
 
+    /**
+     * 解决基本字符串查找问题
+     *
+     * @param pat 匹配模式
+     * @param txt 文本
+     * @return 第一次出现的位置
+     */
     public static int search(String pat, String txt) {
         int M = pat.length();
         int N = txt.length();
 
         for (int i = 0; i < N - M; i++) {
-            int j;
-            for (j = 0; j < M; j++) {
-                if (txt.charAt(i + j) != pat.charAt(j)) {
+            int patIndex;//模式串中的指针
+            for (patIndex = 0; patIndex < M; patIndex++) {
+                //只要遇到不匹配字符,文本指针先前挪动一位.模式指针归零.
+                if (txt.charAt(i + patIndex) != pat.charAt(patIndex)) {
                     break;
                 }
             }
-            if (j == M) {
+
+            //模式完全匹配
+            if (patIndex == M) {
                 return i;
             }
         }
         return END_POSITION;
     }
 
+    /**
+     * 暴力查找的另一种表示,为kmp铺路
+     *
+     * @param pat 匹配模式
+     * @param txt 文本
+     * @return 第一次匹配到的位置
+     */
     public static int reSearch(String pat, String txt) {
-        int txtIndex, patIndex;
+        int txtIndex;
+        int patIndex;
         int M = pat.length();
         int N = txt.length();
+
         for (txtIndex = 0, patIndex = 0; txtIndex < N && patIndex < M; txtIndex++) {
             if (txt.charAt(txtIndex) == pat.charAt(patIndex)) {
                 patIndex++;
-            } else {
+            } else {//如果不匹配,文本下标回到模式串的开头,然后模式串归零
                 txtIndex -= patIndex;
                 patIndex = 0;
             }
         }
 
+        //上面的循环完成后,模式串下标=模式串长度,就说明找到了,返回文本下标,否者没有找到.
         if (patIndex == M) {
             return txtIndex - M;
         } else {
@@ -44,7 +64,7 @@ public class SimpleSearch {
 
     public static void main(String[] args) {
         String txt = "pjrbadmapijqwpjrwqrj";
-        String pat = "baad";
+        String pat = "adm";
         int search = search(pat, txt);
         System.out.println(search);
 
