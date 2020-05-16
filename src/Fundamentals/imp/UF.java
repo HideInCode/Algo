f46e870e72dd123dd5bdd9fc179efa1bd74fdb7f
shh <shh@qq.com>
2020-05-16 16:56:34 +0800

    平衡的并查集

--- src/Fundamentals/imp/UF.java
+++ src/Fundamentals/imp/UF.java
@@ -3,48 +3,80 @@ package Fundamentals.imp;
 import Fundamentals.utils.StdIn;
 import Fundamentals.utils.StdOut;
 
+import java.util.Arrays;
+
 /**
  * union-find
+ * 解决节点连通性检测问题
+ * 使用线性结构会是的union到达O(n) 合并n个会到达O(n^2)
+ * 使用简单的树结构,可能会使得树变成线性的,从而也是O(n)
+ * 使用加权树 保证平衡性 O(lgN) perfect! 即:路径压缩
  */
 public class UF {
+    //下标是当前节点编号,值是父节点的编号
     private int[] id;
+    //记录分量大小,用于压缩路径
+    private int[] size;
     private int count;
-
+    
     public UF(int N) {
         count = N;
         id = new int[N];
         for (int i = 0; i < N; i++) {
             id[i] = i;
         }
+        size = new int[N];
+        Arrays.fill(size, 1);
     }
-
+    
     public int count() {
         return count;
     }
-
+    
+    /**
+     * 并查集的目的就是检验连通性的
+     *
+     * @param p
+     * @param q
+     * @return
+     */
     public boolean connected(int p, int q) {
-        return find(p) == find(q);
+        return root(p) == root(q);
     }
-
+    
     public int find(int p) {
-        return id[p];
+        return root(p);
     }
-
+    
+    //找到根节点的值
+    private int root(int i) {
+        //祖先不是自己
+        while (i != id[i]) {
+            //找祖先
+            i = id[i];
+        }
+        return i;
+    }
+    
+    //添加时保证是一颗平衡树
     public void union(int p, int q) {
-        int pID = find(p);
-        int qID = find(q);
-        if (pID == qID) {
+        int pRoot = root(p);
+        int qRoot = root(q);
+        if (pRoot == qRoot) {
             return;
         }
-
-        for (int i = 0; i < id.length; i++) {
-            if (id[i] == pID) {
-                id[i] = qID;
-            }
+        
+        if (size[pRoot] > size[qRoot]) {
+            id[qRoot] = pRoot;
+            size[qRoot] += size[pRoot];
+        } else {
+            id[pRoot] = qRoot;
+            size[pRoot] += size[qRoot];
         }
+        id[pRoot] = qRoot;
         count--;
     }
-
+    
     public static void main(String[] args) {
         int N = StdIn.readInt();
         UF uf = new UF(N);
@@ -54,7 +86,7 @@ public class UF {
             if (uf.connected(p, q)) {
                 continue;
             }
-
+            
             uf.union(p, q);
             StdOut.println(p + "" + q);
         }
